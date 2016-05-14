7d713e7050e3bb4df7388f379fb89c9bd9513950
delaroy <bamoyk@yahoo.com>
2016-05-14 08:43:56 +0100

    In App Billing
    In App Billing

---
+++ app/src/main/java/com/delaroystudios/inappbilling/InAppBillingActivity.java
@@ -0,0 +1,127 @@
+package com.delaroystudios.inappbilling;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.delaroystudios.inappbilling.util.IabHelper;
+import com.delaroystudios.inappbilling.util.IabResult;
+import com.delaroystudios.inappbilling.util.Inventory;
+import com.delaroystudios.inappbilling.util.Purchase;
+
+public class InAppBillingActivity extends AppCompatActivity {
+
+    private static final String TAG =
+            "InAppBilling";
+    IabHelper mHelper;
+    static final String ITEM_SKU = "com.example.buttonclick";
+
+    private Button clickButton;
+    private Button buyButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_app_billing);
+
+        buyButton = (Button)findViewById(R.id.buyButton);
+        clickButton = (Button)findViewById(R.id.clickButton);
+        clickButton.setEnabled(false);
+        String base64EncodedPublicKey =
+                "<place your public key here>";
+
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+
+        mHelper.startSetup(new
+                                   IabHelper.OnIabSetupFinishedListener() {
+                                       public void onIabSetupFinished(IabResult result) {
+                                           if (!result.isSuccess()) {
+                                               Log.d(TAG, "In-app Billing setup failed: " +
+                                                       result);
+                                           } else {
+                                               Log.d(TAG, "In-app Billing is set up OK");
+                                           }
+                                       }
+                                   });
+    }
+
+    public void buttonClicked (View view)
+    {
+        clickButton.setEnabled(false);
+        buyButton.setEnabled(true);
+    }
+
+    public void buyClick(View view) {
+        mHelper.launchPurchaseFlow(this, ITEM_SKU, 10001,
+                mPurchaseFinishedListener, "mypurchasetoken");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data)
+    {
+        if (!mHelper.handleActivityResult(requestCode,
+                resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener
+            = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result,
+                                          Purchase purchase)
+        {
+            if (result.isFailure()) {
+                // Handle error
+                return;
+            }
+            else if (purchase.getSku().equals(ITEM_SKU)) {
+                consumeItem();
+                buyButton.setEnabled(false);
+            }
+
+        }
+    };
+    public void consumeItem() {
+        mHelper.queryInventoryAsync(mReceivedInventoryListener);
+    }
+
+    IabHelper.QueryInventoryFinishedListener mReceivedInventoryListener
+            = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result,
+                                             Inventory inventory) {
+
+            if (result.isFailure()) {
+                // Handle failure
+            } else {
+                mHelper.consumeAsync(inventory.getPurchase(ITEM_SKU),
+                        mConsumeFinishedListener);
+            }
+        }
+    };
+
+    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener =
+            new IabHelper.OnConsumeFinishedListener() {
+                public void onConsumeFinished(Purchase purchase,
+                                              IabResult result) {
+
+                    if (result.isSuccess()) {
+                        clickButton.setEnabled(true);
+                    } else {
+                        // handle error
+                    }
+                }
+            };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mHelper != null) mHelper.dispose();
+        mHelper = null;
+    }
+
+
+}
